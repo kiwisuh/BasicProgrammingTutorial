daa7118bd42ed842582b0d44778600796d0d1e8c
doorim <dha3@ocdsb.ca>
2018-07-07 15:35:53 -0400

    yeee

--- Strings_Fileio_Classes/Fileio/fileInputTutorial.java
+++ Strings_Fileio_Classes/Fileio/fileInputTutorial.java
@@ -8,5 +8,24 @@ public class fileInputTutorial
 {
 	public static void main(String[] args)
 	{
+		String filename;
+		Scanner in = new Scanner(System.in);
+		System.out.println("name");
+		filename = in.next();
+		try{
+			FileInputStream a = new FileInputStream(filename);
+			FileWriter b = new FileWriter("asdf.txt");
+			Scanner file = new Scanner(a);
+			while (file.hasNext()){
+				String data = file.nextLine();
+				b.writeLine(data);
+				System.out.println(data);
+			}
+			if(b != null){
+				b.close();
+			}
+		}catch(IOException e){
+			System.out.println(e);
+		}
   }
 }
