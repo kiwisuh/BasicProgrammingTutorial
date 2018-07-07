8f61145b16b0d9dbb7dec171115627467a0a5cd5
doorim <dha3@ocdsb.ca>
2018-07-07 13:42:03 -0400

    asd

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,16 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println(args[0]);
+
+        System.out.println(div(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
         //System.out.println("HelloWorld");
     }
 
+    public static int mult (int a, int b){
+      return a*b;
+    }
+
+    public static int div (int a, int b){
+      return a/b;
+    }
 }
